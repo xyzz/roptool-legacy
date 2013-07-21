129b03611820d45ffc6ece780916f1bb299091e3
Davee <davee@x-fusion.co.uk>
2013-07-21 17:42:01 +0100

    Added minimal command line parsing. Can recognise the use of the --help flag.

--- src/RopTool.cpp
+++ src/RopTool.cpp
@@ -2,8 +2,60 @@
 // roptool
 #include "RopTool.h"
 
+// std
+#include <iostream>
+
+// namespace for ease of reading (and writing)
+namespace po = boost::program_options;
+
+RopTool::cmd_options RopTool::get_options(void)
+{
+    // check if there are any options already created by this function
+    if (m_options)
+    {
+        // options already set, just return
+        return m_options;
+    }
+    
+    // create new options
+    m_options.reset(new po::options_description());
+    
+    m_options->add_options()
+        ("help,h", "Show this help dialog.");
+        
+    return m_options;
+}
+        
 int RopTool::start(int argc, char *argv[])
 {
+    // get commandline options
+    cmd_options options = get_options();
+    
+    try
+    {
+        // parse the command line
+        po::store(po::parse_command_line(argc, argv, *options.get()), m_vm);
+        po::notify(m_vm);
+        
+        // check for no args or the help
+		if (m_vm.size() == 0 || m_vm.count("help"))
+		{
+			// show help
+			std::cout << *options.get() << std::endl;
+			return 1;
+		}
+    }
+ 
+    // catch any exceptions
+    catch(std::exception& e) 
+    {
+        // display the exception
+        std::cerr << "Error: " << e.what() << std::endl;
+        std::cerr << "Use --target [target] --help for usage information for a target" << std::endl;
+        std::cerr << "Else, pass no args for generic usage" << std::endl;
+        return 1;
+    }
+    
     return 0;
 }
 
--- src/RopTool.h
+++ src/RopTool.h
@@ -1,6 +1,26 @@
+#ifndef _ROPTOOL_H_
+#define _ROPTOOL_H_
+
+// boost
+#include <boost/program_options.hpp>
+
+// std
+#include <memory>
 
 class RopTool
 {
     public:
         int start(int args, char *argv[]);
+        
+    private:
+        typedef std::shared_ptr<boost::program_options::options_description> cmd_options;
+        
+        // setup the command line options
+        cmd_options get_options(void);
+        
+        // we have a variable map and options
+        boost::program_options::variables_map m_vm;
+        cmd_options m_options;
 };
+
+#endif // _ROPTOOL_H_
