a20950ca84523ffab74f6ecea6c3fb3a50f87acd
Davee <davee@x-fusion.co.uk>
2013-07-26 04:50:13 +0100

    Added significantly more parsing power to the parser. It can now parse down "data" and "code" sections.

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <algorithm>
 #include <boost/spirit/include/qi.hpp>
 #include <boost/spirit/include/phoenix_core.hpp>
 #include <boost/spirit/include/phoenix_operator.hpp>
@@ -14,6 +15,7 @@
 #include <boost/spirit/include/phoenix_stl.hpp>
 #include <boost/spirit/include/phoenix_object.hpp>
 #include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/fusion/include/std_pair.hpp>
 
 #include <boost/spirit/include/support_multi_pass.hpp>
 #include <boost/spirit/include/classic_position_iterator.hpp>
@@ -24,9 +26,27 @@ namespace ascii = boost::spirit::ascii;
 namespace phoenix = boost::phoenix;
 
 BOOST_FUSION_ADAPT_STRUCT(
-    FuncDecl,
+    DataDecl,
+    (func_map, functions)
+    (symbol_map, symbols)
+);
+
+BOOST_FUSION_ADAPT_STRUCT(
+    CallDecl,
+    (std::string, callee)
+    (std::vector<parameter>, parameter_list)
+);
+
+BOOST_FUSION_ADAPT_STRUCT(
+    CodeDecl,
     (std::string, name)
-    (unsigned int, number)
+    (std::vector<CallDecl>, call_list)
+);
+
+BOOST_FUSION_ADAPT_STRUCT(
+    RopScript,
+    (DataDecl, data)
+    (CodeDecl, code)
 );
 
 template <typename Iterator>
@@ -46,48 +66,73 @@ struct skip_grammar : qi::grammar<Iterator>
 };
 
 template <typename Iterator>
-struct ropscript_grammar : qi::grammar<Iterator, FuncDecl(), skip_grammar<Iterator>>
+struct ropscript_grammar : qi::grammar<Iterator, RopScript(), skip_grammar<Iterator>>
 {
-    ropscript_grammar() : ropscript_grammar::base_type(func_decl, "func_decl")
+    ropscript_grammar() : ropscript_grammar::base_type(ropscript, "ropscript")
     {
+        // top level script grammar
+        ropscript = -data_section >> -code_section;
+        
+        // code section rules
+        code_section = qi::lit("code") > -(qi::lit(':') > identifier) > '{' > *call_decl > '}';
+        call_decl = identifier > '(' > -parameter_list > qi::lit(')') > qi::lit(';');
+        parameter_list = param % qi::lit(',');
+        param = quoted_string | number | identifier;
+        
+        // data section rules
+        data_section = qi::lit("data") > '{' > *func_decl >  *symbol_decl > '}';
+        func_decl = qi::lit("func") > identifier > '=' > number > ';';
+        symbol_decl = qi::lit("symbol") > identifier > '=' > (number | string)  > ';';
+        
         // define what is classed as an identifier
         // this will include function call names, variable names
         // symbol names and so forth
-        func_decl = qi::lit("func") > identifier > '=' > number > ';';
-        
         identifier =  qi::lexeme[letter >> *(letter | decimal_digit)];
         decimal = qi::int_;
         hex = (qi::lit("0x") | qi::lit("0X")) >> qi::hex;
         octal = qi::lit("0") >> qi::oct;
         number = hex | octal | decimal;
+        string = qi::lexeme['"' >> +(qi::char_ - '"') >> '"'];
+        quoted_string = qi::lexeme[qi::char_('"') >> +(qi::char_ - '"') >> qi::char_('"')];
         
         // basic rules define what is a letter and what is a number
         letter = qi::char_("a-zA-Z_");
         decimal_digit = qi::char_("0-9");
         
         // name the rules
+        ropscript.name("ropscript");
+        code_section.name("code_section");
+        call_decl.name("call_decl");
+        parameter_list.name("parameter_list");
+        param.name("param");
+        data_section.name("data_section");
+        symbol_decl.name("symbol_decl");
         func_decl.name("func_decl");
-        identifier.name("identifier");  
+        identifier.name("identifier");
+        string.name("string");
+        quoted_string.name("quoted_string");
+        number.name("number");
         letter.name("letter");
         decimal_digit.name("decimal_digit");
     }
     
-    qi::rule<Iterator, char()> decimal_digit;
-    qi::rule<Iterator, char()> letter;
-    qi::rule<Iterator, int()> decimal;
-    qi::rule<Iterator, int()> octal;
-    qi::rule<Iterator, int()> hex;
-    qi::rule<Iterator, int()> number;
-    qi::rule<Iterator, std::string()> identifier;
-    qi::rule<Iterator, std::string()> simple2;
-    qi::rule<Iterator, FuncDecl(), skip_grammar<Iterator>> func_decl;
-    qi::rule<Iterator, FuncDecl()> ropscript;
+    qi::rule<Iterator, char()> decimal_digit, letter;
+    qi::rule<Iterator, int()> decimal, octal, hex, number;
+    qi::rule<Iterator, std::string()> identifier, string, quoted_string;
+    qi::rule<Iterator, func_pair(), skip_grammar<Iterator>> func_decl;
+    qi::rule<Iterator, symbol_pair(), skip_grammar<Iterator>> symbol_decl;
+    qi::rule<Iterator, DataDecl(), skip_grammar<Iterator>> data_section;
+    qi::rule<Iterator, parameter(), skip_grammar<Iterator>> param;
+    qi::rule<Iterator, std::vector<parameter>(), skip_grammar<Iterator>> parameter_list;
+    qi::rule<Iterator, CallDecl(), skip_grammar<Iterator>> call_decl;
+    qi::rule<Iterator, CodeDecl(), skip_grammar<Iterator>> code_section;
+    qi::rule<Iterator, RopScript(), skip_grammar<Iterator>> ropscript;
 };
 
 bool parse(const char *filename)
 {
     std::ifstream ifs;
-    FuncDecl out;
+    RopScript out;
     
     // open file
     ifs.open(filename, std::ios_base::in);
@@ -144,6 +189,27 @@ bool parse(const char *filename)
         return false;
     }
 
-    std::cout << "out: " << out.name << " addr: " << out.number << "\n";
+    std::cout << "data:\n";
+    std::for_each(out.data.functions.begin(), out.data.functions.end(), [=](const func_pair& p)
+    {
+        std::cout << "\tfunction: " << p.first << " address: " << p.second << "\n";
+    });
+ 
+    std::for_each(out.data.symbols.begin(), out.data.symbols.end(), [=](const symbol_pair& p)
+    {
+        std::cout << "\tsymbol: " << p.first << " value: " << p.second << "\n";
+    });
+    
+    std::cout << "code: " << out.code.name << "\n";
+    std::for_each(out.code.call_list.begin(), out.code.call_list.end(), [=](const CallDecl& p)
+    {
+        std::cout << "\tcall: " << p.callee << "\n";
+        std::cout << "\tparams: " << p.parameter_list.size() << "\n";
+        std::for_each(p.parameter_list.begin(), p.parameter_list.end(), [=](const parameter& p)
+        {
+            std::cout << "\t\tparam: " << p << "\n";
+        });
+    });
+    
     return r;
 }
