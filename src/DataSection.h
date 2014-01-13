98f4583c002d7e74c401b8c5b45ba624ba0dbf65
David Morgan <davee@x-fusion.co.uk>
2014-01-13 20:48:39 +0000

    Added DataRef handler notification when base address is changed.

--- src/DataSection.h
+++ src/DataSection.h
@@ -34,6 +34,14 @@ class DataRef
 		// only friends can construct these
 		DataRef(u64 ref_id) { m_ref_id = ref_id; }
 		
+		void onUpdate(DataRefPtr ref)
+		{
+			std::for_each(m_handler.begin(), m_handler.end(), [=](DataRefHandler handler)
+			{
+				handler(ref);
+			});
+		}
+		
 		std::list<DataRefHandler> m_handler;
 		u64 m_ref_id, m_val;
 };
@@ -44,6 +52,18 @@ class DataSection
 		DataSection(void)
 		{
 			m_ref_id_count = 1;
+		}
+		
+		void setBase(u64 base_address)
+		{
+			// go through whole list
+			std::for_each(m_update_list.begin(), m_update_list.end(), [=](DataRefPtr ref)
+			{
+				ref->set(base_address+(ref->value()-m_base_address));
+				ref->onUpdate(ref);
+			});
+			
+			m_base_address = base_address;
 		}
 		
 		DataRefPtr add(u64 constant)
@@ -64,6 +84,9 @@ class DataSection
 			// set an update notification
 			m_update_list.push_back(ref);
 			
+			// get current position
+			u64 offset = m_data.size();
+			
 			// allocate memory
 			std::vector<unsigned char> data_temp(str.size());
 			std::transform(str.begin(), str.end(), data_temp.begin(), [=](char c)
@@ -73,11 +96,14 @@ class DataSection
 			
 			m_data.insert(m_data.end(), data_temp.begin(), data_temp.end());
 			m_data.push_back('\0');
+			
+			// set value
+			ref->set(m_base_address + offset);
 			return ref;
 		}
 		
 	private:
-		u64 m_ref_id_count;
+		u64 m_ref_id_count, m_base_address;
 		std::vector<unsigned char> m_data;
 		std::list<DataRefPtr> m_update_list;
 };
